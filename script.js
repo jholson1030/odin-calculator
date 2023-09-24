285b99fccff950862d7f3afc476d1d1c0ffc11ea
John Olson <jholson1030@gmail.com>
2023-09-24 05:53:31 -0500

    Create besic functions for +, -, *, and /

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function operate(num1, num2) {
 function populate() {
     // This function will make the buttons clicked display on the output div
 }
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
