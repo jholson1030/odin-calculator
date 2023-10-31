929b427dc12d970447829fdd4dba301350527c40
John Olson <jholson1030@gmail.com>
2023-10-31 00:27:06 -0500

    Write more tests

--- calculator.test.js
+++ calculator.test.js
@@ -5,3 +5,27 @@ test('should correctly calculate a complex expression', () => {
     const result = operate(expression);
     expect(result).toBe('4');
   });
+
+  test('should handle addition', () => {
+    const expression = ['5', '+', '7'];
+    const result = operate(expression);
+    expect(result).toBe('12');
+  });
+
+  test('should handle subtraction', () => {
+    const expression = ['10', '-', '3'];
+    const result = operate(expression);
+    expect(result).toBe('7');
+  });
+
+  test('should handle multiplication', () => {
+    const expression = ['12', '*', '5'];
+    const result = operate(expression);
+    expect(result).toBe('60');
+  });
+
+  test('should handle division', () => {
+    const expression = ['42', '/', '7'];
+    const result = operate(expression);
+    expect(result).toBe('6');
+  });
