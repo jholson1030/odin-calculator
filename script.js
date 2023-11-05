556431d3beb5852e6e585225e8a8d6c7ae952c94
John Olson <jholson1030@gmail.com>
2023-11-04 23:56:55 -0500

    Remove querySelectors for individual numbers

--- script.js
+++ script.js
@@ -6,16 +6,6 @@ let secondNum = '';
 let display = document.querySelector('#display');
 let displayValue = '';
 
-let zero = document.querySelector('.zero');
-let one = document.querySelector('.one');
-let two = document.querySelector('.two');
-let three = document.querySelector('.three');
-let four = document.querySelector('.four');
-let five = document.querySelector('.five');
-let six = document.querySelector('.six');
-let seven = document.querySelector('.seven');
-let eight = document.querySelector('.eight');
-let nine = document.querySelector('.nine');
 
 
 
